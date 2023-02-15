617e0f5686aa120f59790654bfce580ff9d405ca
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-02-14 20:09:28 -0800

    Add common cells to DR and TTN, plus WIP TTN scripts

--- task_trained_network/oct22_tt_stim_script.py
+++ task_trained_network/oct22_tt_stim_script.py
@@ -7,6 +7,18 @@ from camstim import Foraging
 from camstim import Window, Warp
 import numpy as np
 
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('params_path', nargs='?', type=str, default='', )
+args, _ = parser.parse_known_args()
+
+with open(args.params_path, 'r') as f:
+    json_params = json.load(f)
+    
+# setup main stim ----------------------------------------------------------------------
+
 # Create display window
 window = Window(fullscr=True,
                 monitor='GammaCorrect30', #MUST CONFIRM: value in `change_detection_extended` = 'Gamma1.Luminance50'
@@ -17,30 +29,15 @@ window = Window(fullscr=True,
 movie_path = 'C:\\ProgramData\\StimulusFiles\\dev\\'
 
 session = 1
+
+old, reversed, annotated = (json_params['stim_repeats'][key] for key in ('old', 'reversed', 'annotated'))
 segment_secs = (
-    [('old_stim.stim', 40)]*25 +
-    [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),]*8 +
-    [('densely_annotated_%02d.stim'%i, 60) for i in range(19)] * 2 +
-    [('old_stim.stim', 40)]*25 +
-    [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),]*8
-)  # 6200 seconds = 103 minutes, 20 secs
-
-hab_60 = (15, 5, 1)
-hab_90 = (20, 7, 2)
-hab_120 = (32, 10, 2)
-exp_103 = (25, 8, 2)
-
-def file_secs(repeats):
-    old, reversed, annotated = repeats
-    return (
-        [('old_stim.stim', 40)]*old + 
-        [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),]*reversed + 
-        [('densely_annotated_%02d.stim'%i, 60) for i in range(19)] * annotated + 
-        [('old_stim.stim', 40)]*old + 
-        [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),]*reversed
+    [('old_stim.stim', 40)]*old + 
+    [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),]*reversed + 
+    [('densely_annotated_%02d.stim'%i, 60) for i in range(19)] * annotated + 
+    [('old_stim.stim', 40)]*old + 
+    [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),]*reversed
 )
-
-segment_secs = file_secs(exp_103) # 6200 seconds = 103 minutes, 20 secs
 
 num_segments = len(segment_secs)
 
@@ -48,8 +45,6 @@ num_segments = len(segment_secs)
 frames_per_sec = 30.0
 stim = []
 total_movie_sec = 0
-import os
-total_movie_bytes = 0
 
 for file, length in segment_secs:
     segment = Stimulus.from_file(movie_path+file, window)
@@ -57,10 +52,7 @@ for file, length in segment_secs:
     segment.set_display_sequence(segment_ds)
     
     total_movie_sec += length
-    total_movie_bytes += os.stat(movie_path+file).st_size
     stim.append(segment)
-
-print total_movie_bytes
 
 params = {
 	    'syncsqr': True,
@@ -71,9 +63,9 @@ params = {
         'syncpulselines': [5, 6],  # frame, start/stop
         'trigger_delay_sec': 5.0,
     }
-
+            
 # create SweepStim instance
-ss = SweepStim(window,
+main_stim = SweepStim(window,
                stimuli=stim,
                pre_blank_sec=2,
                post_blank_sec=2,
@@ -84,9 +76,90 @@ ss = SweepStim(window,
 f = Foraging(window=window,
              auto_update=False,
              params=params,
-             nidaq_tasks={'digital_input': ss.di,
-                          'digital_output': ss.do,})  #share di and do with SS
-ss.add_item(f, "foraging")
+             nidaq_tasks={'digital_input': main_stim.di,
+                          'digital_output': main_stim.do,})  #share di and do with SS
+main_stim.add_item(f, "foraging")
+# postpone running until we've set up the other stim objects
 
-# run it
-ss.run()
+
+# setup mapping ----------------------------------------------------------------------------
+# TODO build mapping object with json_params
+if not json_params.get('mapping'):
+    RUN_MAPPING = False
+else:
+    RUN_MAPPING = True
+        
+    """from spontaneous_stimulus_dv.py"""
+
+    # Create display window
+    mapping_window = Window(fullscr=True,
+                    monitor= 'Gamma1.Luminance50',
+                    screen=0,
+                    warp=Warp.Spherical,)
+
+
+    # paths to our stimuli
+    g20_path = 		"C:/ecephys_stimulus_scripts/gabor_20_deg_250ms.stim" # 20 minutes (1200 s)
+    fl250_path =	"C:/ecephys_stimulus_scripts/flash_250ms.stim" # 5 minutes (300 s)
+
+    # load our stimuli
+    g20 = Stimulus.from_file(g20_path, mapping_window)
+    fl250 = Stimulus.from_file(fl250_path, mapping_window)
+
+    g20_ds = [(0, 1200)]
+    fl250_ds = [(1200, 1500)]
+
+    g20.set_display_sequence(g20_ds)
+    fl250.set_display_sequence(fl250_ds)
+
+    # kwargs
+    mapping_params = {
+        'syncpulse': True,
+        'syncpulseport': 1,
+        'syncpulselines': [4, 7],  # frame, start/stop
+        'trigger_delay_sec': 0.0,
+        'bgcolor': (-1,-1,-1),
+        'eyetracker': False,
+        'eyetrackerip': "W7DT12722", #! np.0 mon is w10dtsm112722
+        'eyetrackerport': 1000,
+        'syncsqr': True,
+        'syncsqrloc': (0,0),
+        'syncsqrfreq': 60,
+        'syncsqrsize': (100,100),
+        'showmouse': True
+    }
+
+    # create SweepStim instance
+    mapping_stim = SweepStim(mapping_window,
+                stimuli=[g20, fl250],
+                pre_blank_sec=0,
+                post_blank_sec=0,
+                params=mapping_params,
+                )
+
+    # add in foraging so we can track wheel, potentially give rewards, etc
+    f = Foraging(window=mapping_window,
+                auto_update=False,
+                params=mapping_params,
+                nidaq_tasks={'digital_input': mapping_stim.di,
+                            'digital_output': mapping_stim.do,})  #share di and do with SS
+    mapping_stim.add_item(f, "foraging")
+    
+
+# setup opto -----------------------------------------------------------------------------
+# TODO 
+if not json_params.get('opto'):
+    RUN_OPTO = False
+else:
+    RUN_OPTO = True
+    
+# run stims
+
+if RUN_MAPPING:
+    mapping_stim.run()
+
+main_stim.run()
+
+if RUN_OPTO:
+    pass
+
---
+++ task_trained_network/ttn_widgets.py
@@ -0,0 +1,110 @@
+import dataclasses
+import datetime
+import enum
+import pathlib
+import threading
+import time
+from typing import ClassVar, NamedTuple, NoReturn, Optional, TypedDict
+
+import IPython
+import IPython.display
+import ipywidgets as ipw
+import np_config
+import np_logging
+import np_session
+import np_services
+import PIL.Image
+import pydantic
+
+import np_workflows.npxc as npxc
+
+logger = np_logging.getLogger(__name__)
+
+
+class TTNSession(enum.Enum):
+    """Enum for stimulus sessions."""
+    PRETEST = 'pretest'
+    HAB_60 = 'hab 60'
+    HAB_90 = 'hab 90'
+    HAB_120 = 'hab 120'
+    ECEPHYS = 'ecephys'
+
+
+def stim_repeats(old: int, reversed: int, annotated: int) -> dict[str, dict[str, int]]:
+    return {'stim_repeats': dict(old=old, reversed=reversed, annotated=annotated)}
+
+def opto_params(pretest=False):
+    params = dict(level_list=[1.15, 1.28, 1.345])
+    if pretest: 
+        params.update(**dict(operation_mode='pretest'))
+    else: 
+        params.update(**dict(operation_mode='experiment'))
+    return {'opto': params}
+
+def mapping_params(pretest=False):
+    return {}
+
+class TTNSelectedSession:
+    
+    common_params: ClassVar[dict] = {}
+    "Will be updated with `session_params` when a session is selected."
+    
+    session_params: ClassVar[dict[TTNSession, dict]] = {
+        TTNSession.PRETEST: {**stim_repeats(1, 1, 1), **opto_params(pretest=True), **mapping_params(pretest=True)},
+        TTNSession.HAB_60: {**stim_repeats(15, 5, 1)},
+        TTNSession.HAB_90: {**stim_repeats(20, 7, 2)},
+        TTNSession.HAB_120: {**stim_repeats(25, 8, 2), **mapping_params()}, # stim-only is 103 mins
+        TTNSession.ECEPHYS: {**stim_repeats(25, 8, 2),  **opto_params(), **mapping_params()}, 
+    }
+    
+    opto_script: ClassVar[str] = 'C:/ProgramData/StimulusFiles/dev/opto_tagging_v2.py'
+    
+    def __init__(self, session: str | TTNSession):
+        if isinstance(session, str):
+            session = TTNSession(session)
+        self.session = session
+        
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(session={self.session})'
+    
+    @property
+    def script(self) -> str:
+        logger.warning(f'Using hard-coded script in notebooks directory for testing')
+        # will eventually point to 'C:/ProgramData/StimulusFiles/dev/oct22_tt_stim_script.py'
+        return pathlib.Path('oct22_tt_stim_script.py').resolve().as_posix()
+    
+    @property
+    def params(self) -> dict:
+        return self.__class__.common_params | self.__class__.session_params[self.session]
+
+        
+    
+def stim_session_select_widget() -> TTNSelectedSession:
+    """Select a stimulus session (hab, pretest, ecephys) to run."""
+    
+    selection = TTNSelectedSession(TTNSession.PRETEST)
+       
+    session_dropdown = ipw.Select(
+        options = tuple(_.value for _ in TTNSession),
+        description = 'Session',
+    )
+    console = ipw.Output()
+    with console:
+        print(f'Selected: {selection.session}')
+    
+    def update(change):
+        if change['name'] != 'value':
+            return
+        if (options := getattr(change['owner'], 'options', None)) and change['new'] not in options:
+            return
+        if change['new'] == change['old']:
+            return
+        selection.__init__(str(session_dropdown.value))
+        with console:
+            print(f'Selected: {selection.session}')
+            
+    session_dropdown.observe(update)
+    
+    IPython.display.display(ipw.VBox([session_dropdown, console]))
+    
+    return selection
