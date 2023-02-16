33e7ab4ba56611126d229c16f0eb107fe252b423
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-02-16 13:23:45 -0800

    Update TTN script/param specification

--- task_trained_network/oct22_tt_stim_script.py
+++ task_trained_network/oct22_tt_stim_script.py
@@ -7,8 +7,12 @@ from camstim import Foraging
 from camstim import Window, Warp
 import numpy as np
 
+import logging
 import argparse
 import json
+
+# get params for mapping/opto/main from a single json file, specified by command line
+# arg 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('params_path', nargs='?', type=str, default='', )
@@ -17,32 +21,36 @@ args, _ = parser.parse_known_args()
 with open(args.params_path, 'r') as f:
     json_params = json.load(f)
     
-# setup main stim ----------------------------------------------------------------------
+# setup main stim
+# -----------------------------------------------------------------------
+
+main_params = json_params['main']
 
 # Create display window
 window = Window(fullscr=True,
-                monitor='GammaCorrect30', #MUST CONFIRM: value in `change_detection_extended` = 'Gamma1.Luminance50'
+                monitor=main_params['monitor'], #MUST CONFIRM: value used in other scripts = 'Gamma1.Luminance50'
                 screen=0,
                 warp=Warp.Disabled
                 )
 
-movie_path = 'C:\\ProgramData\\StimulusFiles\\dev\\'
+movie_path = main_params['movie_path']
 
-session = 1
+session = main_params['session']                    #! unused
 
-old, reversed, annotated = (json_params['stim_repeats'][key] for key in ('old', 'reversed', 'annotated'))
+old, reversed, annotated = (main_params['stim_repeats'][key] for key in ('old', 'reversed', 'annotated'))
 segment_secs = (
-    [('old_stim.stim', 40)]*old + 
-    [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),]*reversed + 
+    [('old_stim.stim', 40)] * old + 
+    [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),] * reversed + 
     [('densely_annotated_%02d.stim'%i, 60) for i in range(19)] * annotated + 
-    [('old_stim.stim', 40)]*old + 
-    [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),]*reversed
+    [('old_stim.stim', 40)] * old + 
+    [('shuffle_reversed.stim', 40), ('shuffle_reversed_1st.stim', 40), ('shuffle_reversed_2nd.stim', 40),] * reversed
 )
 
 num_segments = len(segment_secs)
 
+frames_per_sec = main_params['frames_per_sec']      #! unused
+
 #build the stimulus array
-frames_per_sec = 30.0
 stim = []
 total_movie_sec = 0
 
@@ -54,88 +62,70 @@ for file, length in segment_secs:
     total_movie_sec += length
     stim.append(segment)
 
-params = {
-	    'syncsqr': True,
-        'syncsqrloc': (875,550),
-        'syncsqrsize': (150,150),
-        'syncpulse': True,
-        'syncpulseport': 1,
-        'syncpulselines': [5, 6],  # frame, start/stop
-        'trigger_delay_sec': 5.0,
-    }
-            
 # create SweepStim instance
 main_stim = SweepStim(window,
                stimuli=stim,
                pre_blank_sec=2,
                post_blank_sec=2,
-               params=params,
+               params=main_params['sweepstim'],
                )
 
 # add in foraging so we can track wheel, potentially give rewards, etc
 f = Foraging(window=window,
              auto_update=False,
-             params=params,
+             params=main_params['sweepstim'],
              nidaq_tasks={'digital_input': main_stim.di,
                           'digital_output': main_stim.do,})  #share di and do with SS
+
 main_stim.add_item(f, "foraging")
-# postpone running until we've set up the other stim objects
+
+# postpone running until we've set up all stim objects
 
 
-# setup mapping ----------------------------------------------------------------------------
-# TODO build mapping object with json_params
-if not json_params.get('mapping'):
-    RUN_MAPPING = False
-else:
-    RUN_MAPPING = True
+# setup mapping
+# ----------------------------------------------------------------------------
+mapping_params = json_params['mapping'] # will be empty for some sessions
+
+if mapping_params:
         
     """from spontaneous_stimulus_dv.py"""
 
     # Create display window
     mapping_window = Window(fullscr=True,
-                    monitor= 'Gamma1.Luminance50',
+                    monitor=mapping_params['monitor'],
                     screen=0,
                     warp=Warp.Spherical,)
 
-
-    # paths to our stimuli
-    g20_path = 		"C:/ecephys_stimulus_scripts/gabor_20_deg_250ms.stim" # 20 minutes (1200 s)
-    fl250_path =	"C:/ecephys_stimulus_scripts/flash_250ms.stim" # 5 minutes (300 s)
-
     # load our stimuli
-    g20 = Stimulus.from_file(g20_path, mapping_window)
-    fl250 = Stimulus.from_file(fl250_path, mapping_window)
-
-    g20_ds = [(0, 1200)]
-    fl250_ds = [(1200, 1500)]
-
-    g20.set_display_sequence(g20_ds)
-    fl250.set_display_sequence(fl250_ds)
-
-    # kwargs
-    mapping_params = {
-        'syncpulse': True,
-        'syncpulseport': 1,
-        'syncpulselines': [4, 7],  # frame, start/stop
-        'trigger_delay_sec': 0.0,
-        'bgcolor': (-1,-1,-1),
-        'eyetracker': False,
-        'eyetrackerip': "W7DT12722", #! np.0 mon is w10dtsm112722
-        'eyetrackerport': 1000,
-        'syncsqr': True,
-        'syncsqrloc': (0,0),
-        'syncsqrfreq': 60,
-        'syncsqrsize': (100,100),
-        'showmouse': True
-    }
+    gabor = Stimulus.from_file(mapping_params['gabor_path'], mapping_window)
+    flash = Stimulus.from_file(mapping_params['flash_path'], mapping_window)
+    
+    gabor_duration_sec = mapping_params['gabor_duration_seconds'] 
+    flash_duration_sec = mapping_params['flash_duration_seconds']
+    
+    # if max total duration is set, and less than original movie length, cut down display sequence:
+    max_mapping_duation_minutes = mapping_params["max_total_duration_minutes"] # can be zero, in which case we use the full movie length
+    max_mapping_duration_sec = max_mapping_duation_minutes * 60
+    if 0 < max_mapping_duration_sec < (gabor_duration_sec + flash_duration_sec):
+        
+        logging.debug("Mapping duration capped at %s minutes", max_mapping_duation_minutes)
+        
+        original_duration_sec = gabor_duration_sec + flash_duration_sec
+        
+        gabor_duration_sec = int((gabor_duration_sec / original_duration_sec) * max_mapping_duration_sec)
+        flash_duration_sec = int((flash_duration_sec / original_duration_sec) * max_mapping_duration_sec)
+        
+    gabor.set_display_sequence([(0, gabor_duration_sec)])
+    flash.set_display_sequence([(gabor_duration_sec, gabor_duration_sec + flash_duration_sec)])
 
     # create SweepStim instance
-    mapping_stim = SweepStim(mapping_window,
-                stimuli=[g20, fl250],
-                pre_blank_sec=0,
-                post_blank_sec=0,
-                params=mapping_params,
-                )
+    mapping_stim = SweepStim(
+            mapping_window,
+            stimuli=[gabor, flash],
+            pre_blank_sec=10,
+            post_blank_sec=10,
+            params=mapping_params,
+        )
 
     # add in foraging so we can track wheel, potentially give rewards, etc
     f = Foraging(window=mapping_window,
@@ -143,24 +133,16 @@ else:
                 params=mapping_params,
                 nidaq_tasks={'digital_input': mapping_stim.di,
                             'digital_output': mapping_stim.do,})  #share di and do with SS
+    
     mapping_stim.add_item(f, "foraging")
+    #! equivalent line in mapping_script_v2.py uses 'behavior' instead of 'foraging':
+    # ss.add_item(f, 'behavior')  # makes our output structure items:behavior:...
     
 
-# setup opto -----------------------------------------------------------------------------
-# TODO 
-if not json_params.get('opto'):
-    RUN_OPTO = False
-else:
-    RUN_OPTO = True
-    
+# -----------------------------------------------------------------------------------------
 # run stims
 
-if RUN_MAPPING:
+if mapping_params:
     mapping_stim.run()
 
 main_stim.run()
-
-if RUN_OPTO:
-    pass
-
-#
--- task_trained_network/ttn_pilot.py
+++ task_trained_network/ttn_pilot.py
@@ -1,10 +1,11 @@
+import configparser
 import dataclasses
 import datetime
 import enum
 import pathlib
 import threading
 import time
-from typing import ClassVar, NamedTuple, NoReturn, Optional, TypedDict
+from typing import ClassVar, Literal, NamedTuple, NoReturn, Optional, TypedDict
 
 import IPython
 import IPython.display
@@ -15,37 +16,30 @@ import np_session
 import np_services
 import PIL.Image
 import pydantic
+from pyparsing import Any
 
 import np_workflows.npxc as npxc
 
 logger = np_logging.getLogger(__name__)
 
+# --------------------------------------------------------------------------------------
 
 class TTNSession(enum.Enum):
-    """Enum for stimulus sessions."""
+    """Enum for the different TTN sessions available, each with different param sets."""
     PRETEST = 'pretest'
     HAB_60 = 'hab 60'
     HAB_90 = 'hab 90'
     HAB_120 = 'hab 120'
     ECEPHYS = 'ecephys'
 
+# --------------------------------------------------------------------------------------
 
-def stim_repeats(old: int, reversed: int, annotated: int) -> dict[str, dict[str, int]]:
-    return {'stim_repeats': dict(old=old, reversed=reversed, annotated=annotated)}
+# setup parameters ---------------------------------------------------------------------
+default_ttn_params = {}
 
-def opto_params(pretest=False):
-    params = dict(level_list= [1.35])# [0.77, 0.97, 1.35]
-    if pretest: 
-        params.update(**dict(operation_mode='pretest'))
-    else: 
-        params.update(**dict(operation_mode='experiment'))
-    return {'opto': params}
 
-def mapping_params(pretest=False):
-    return {}
-
-import configparser
-# import np_config
+# camstim defaults ---------------------------------------------------------------------
+# fetched from config file on the Stim computer
 parser = configparser.RawConfigParser()
 parser.read((np_config.Rig().paths['Camstim'].parent / 'config' / 'stim.cfg').as_posix())
 
@@ -60,30 +54,161 @@ for section in parser.sections():
         else:
             camstim_default_config[section][k] = value
 
+default_ttn_params['camstim_defaults'] = camstim_default_config
 
+# main stimulus defaults ---------------------------------------------------------------
+default_ttn_params['main'] = {}
 
+default_ttn_params['main']['sweepstim'] = {
+	    'syncsqr': True,
+        'syncsqrloc': (875,550),
+        'syncsqrsize': (150,150),
+        'syncpulse': True,
+        'syncpulseport': 1,
+        'syncpulselines': [5, 6],  # frame, start/stop
+        'trigger_delay_sec': 5.0,
+    }
+default_ttn_params['main']['movie_path'] = 'C:/ProgramData/StimulusFiles/dev/'
+default_ttn_params['main']['frames_per_sec'] = 30.0         #! unused
+default_ttn_params['main']['session'] = 1                   #! unused
+default_ttn_params['main']['monitor'] = 'GammaCorrect30'    #! MUST CONFIRM: value used in other scripts = 'Gamma1.Luminance50'
+
+# other parameters that vary depending on session type (pretest, hab, ecephys):
+
+def build_session_stim_repeats(old: int, reversed: int, annotated: int) -> dict[str, dict[str, int]]:
+    return {'stim_repeats': dict(old=old, reversed=reversed, annotated=annotated)}
+
+def session_stim_repeats(session: TTNSession) -> dict[str, dict[str, int]]:
+    "`'main'` key in params dict should be updated with the returned dict."
+    match session:
+        case TTNSession.PRETEST:
+            return build_session_stim_repeats(1, 0, 0)
+        case TTNSession.HAB_60:
+            return build_session_stim_repeats(15, 5, 1)
+        case TTNSession.HAB_90:
+            return build_session_stim_repeats(20, 7, 2)
+        case TTNSession.HAB_120 | TTNSession.ECEPHYS:
+            return build_session_stim_repeats(25, 8, 2)
+        case _:
+            raise ValueError(f"Stim repeats not implemented for {session}")
+        
+# optotagging defaults -----------------------------------------------------------------
+
+default_ttn_params['opto'] = {}
+default_ttn_params['opto_script'] = 'C:/ProgramData/StimulusFiles/dev/opto_tagging_v2.py'
+
+# all parameters depend on session type (pretest, hab, ecephys):
+
+def session_opto_params(session: TTNSession, mouse: str | int | np_session.Mouse) -> dict[str, dict[str, str | list[float] | Literal['pretest', 'experiment']]]:
+    "`'opto'` key in params dict should be updated with the returned dict (which will be empty for habs)."
+
+    def opto_mouse_id(mouse_id: str | int | np_session.Mouse) -> dict[str, str]:
+        return {'mouseID': str(mouse_id)}
+
+    def opto_levels(session: TTNSession) -> dict[str, list[float]]:
+        default_opto_levels: list[float] = default_ttn_params['camstim_defaults']['Optogenetics']['level_list']
+        match session:
+            case TTNSession.PRETEST:
+                return {'level_list': [max(default_opto_levels)]}
+            case TTNSession.ECEPHYS:
+                return {'level_list': default_opto_levels}
+            case _:
+                raise ValueError(f"Opto levels not implemented for {session}")
+            
+    def opto_operation_mode(session: TTNSession) -> dict[str, Literal['pretest', 'experiment']]:
+        match session:
+            case TTNSession.PRETEST:
+                return {'operation_mode': 'pretest'}
+            case TTNSession.ECEPHYS:
+                return {'operation_mode': 'experiment'}
+            case _:
+                raise ValueError(f"Opto levels not implemented for {session}")
+            
+    match session:
+        case TTNSession.PRETEST | TTNSession.ECEPHYS:
+            params = {}
+            params.update(opto_mouse_id(mouse))
+            params.update(opto_levels(session))
+            params.update(opto_operation_mode(session))
+            return params
+        case TTNSession.HAB_60 | TTNSession.HAB_90 | TTNSession.HAB_120:
+            return {}
+        case _:
+            raise ValueError(f"Opto params not implemented for {session}")
+        
+        
+# mapping defaults ---------------------------------------------------------------------
+
+default_ttn_params['mapping'] = {}
+
+default_ttn_params['mapping']['monitor'] = 'Gamma1.Luminance50'
+default_ttn_params['mapping']['gabor_path'] = 'gabor_20_deg_250ms.stim' #? c:/programdata/stimulusfiles/dev/
+default_ttn_params['mapping']['flash_path'] = 'flash_250ms.stim'
+default_ttn_params['mapping']['gabor_duration_seconds'] = 1200 
+default_ttn_params['mapping']['flash_duration_seconds'] = 300 
+
+# two alternative sweepstim paramsets from different scripts:
+
+default_ttn_params['mapping']['sweepstim'] = {
+    'syncpulse': True,
+    'syncpulseport': 1,
+    'syncpulselines': [4, 7],  # frame, start/stop
+    'trigger_delay_sec': 0.0,
+    'bgcolor': (-1,-1,-1),
+    'eyetracker': False,
+    'eyetrackerip': "W7DT12722", #! np.0 mon is w10dtsm112722
+    'eyetrackerport': 1000,
+    'syncsqr': True,
+    'syncsqrloc': (0,0),
+    'syncsqrfreq': 60,
+    'syncsqrsize': (100,100),
+    'showmouse': True
+} # from dv_spontaneous_stimulus.py
+
+default_ttn_params['mapping']['sweepstim'] = {
+    
+} # from a DR experiment with mapping_script_v2.py
+
+# all parameters depend on session type (pretest, hab, ecephys):
+def session_mapping_params(session: TTNSession) -> dict[str, dict[str, int]]:
+    "`'mapping'` key in params dict should be updated with the returned dict (which will be empty for habs)."
+    
+    def mapping_duration(session: TTNSession) -> dict[str, float]:
+        # 0 = full length = gabor_duration + flash_duration = maximum possible
+        match session:
+            case TTNSession.PRETEST:
+                return {'max_total_duration_minutes': 0.5}
+            case TTNSession.ECEPHYS:
+                return {'max_total_duration_minutes': 10} 
+            case _:
+                raise ValueError(f"Mapping params not implemented for {session}")
+            
+    match session:
+        case TTNSession.PRETEST | TTNSession.ECEPHYS:
+            params = {}
+            params.update(mapping_duration(session))
+            return params
+        case TTNSession.HAB_60 | TTNSession.HAB_90 | TTNSession.HAB_120:
+            return {}
+        case _:
+            raise ValueError(f"Mapping params not implemented for {session}")
+
+ 
 class TTNSelectedSession:
     
-    common_params: ClassVar[dict] = camstim_default_config
+    common_params: ClassVar[dict] = default_ttn_params
     "Will be updated with `session_params` when a session is selected."
-    
-    session_params: ClassVar[dict[TTNSession, dict]] = {
-        TTNSession.PRETEST: {**stim_repeats(1, 1, 1), **opto_params(pretest=True), **mapping_params(pretest=True)},
-        TTNSession.HAB_60: {**stim_repeats(15, 5, 1)},
-        TTNSession.HAB_90: {**stim_repeats(20, 7, 2)},
-        TTNSession.HAB_120: {**stim_repeats(25, 8, 2), **mapping_params()}, # stim-only is 103 mins
-        TTNSession.ECEPHYS: {**stim_repeats(25, 8, 2),  **opto_params(), **mapping_params()}, 
-    }
     
     opto_script: ClassVar[str] = 'C:/ProgramData/StimulusFiles/dev/opto_tagging_v2.py'
     
-    def __init__(self, session: str | TTNSession):
+    def __init__(self, session: str | TTNSession, mouse: str | int | np_session.Mouse):
         if isinstance(session, str):
             session = TTNSession(session)
         self.session = session
+        self.mouse = str(mouse)
         
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(session={self.session})'
+        return f'{self.__class__.__name__}({self.session}, {self.mouse})'
     
     @property
     def script(self) -> str:
@@ -92,15 +217,22 @@ class TTNSelectedSession:
         return np_config.local_to_unc(np_config.Rig().sync, pathlib.Path('oct22_tt_stim_script.py').resolve()).as_posix()
     
     @property
-    def params(self) -> dict:
-        return self.__class__.common_params | self.__class__.session_params[self.session]
-
+    def session_params(self) -> dict[Literal['main', 'camstim_defaults', 'mapping', 'opto'], dict[str, Any]]:
+        params = self.common_params.copy()
+        params['main'].update(session_stim_repeats(self.session))
+        params['mapping'].update(session_mapping_params(self.session))
+        params['opto'].update(session_opto_params(self.session, self.mouse))
+        return params
+    
+    @property
+    def opto_params(self) -> dict[str, Any]:
+        return self.session_params['opto']
         
     
-def stim_session_select_widget() -> TTNSelectedSession:
+def stim_session_select_widget(mouse: str | int | np_session.Mouse) -> TTNSelectedSession:
     """Select a stimulus session (hab, pretest, ecephys) to run."""
     
-    selection = TTNSelectedSession(TTNSession.PRETEST)
+    selection = TTNSelectedSession(TTNSession.PRETEST, mouse)
        
     session_dropdown = ipw.Select(
         options = tuple(_.value for _ in TTNSession),
@@ -117,7 +249,7 @@ def stim_session_select_widget() -> TTNSelectedSession:
             return
         if change['new'] == change['old']:
             return
-        selection.__init__(str(session_dropdown.value))
+        selection.__init__(str(session_dropdown.value), mouse)
         with console:
             print(f'Selected: {selection.session}')
             
