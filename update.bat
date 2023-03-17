5c425caf812f53705d8ca7a60feb91bcca17194c
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-03-17 15:40:26 -0700

    Move updating venv to standalone script

---
+++ update.bat
@@ -0,0 +1,26 @@
+echo off
+
+cd c:\users\svc_neuropix\documents\github\np_notebooks
+
+call .venv\scripts\activate.bat
+
+echo git reset
+git reset --hard -q
+
+@REM first display the files without deleting, in case the user isn't aware of what this script does
+echo git clean
+git clean -n -f -d
+
+@REM y for yes: default behavior is to delete untracked files in np_notebooks
+set clean=y 
+
+set /p clean=The above files/folders will be deleted. Clean? (y/n) [%clean%]
+IF not %clean%=="n" (
+git clean -f -d
+)
+
+git pull origin main
+
+python -m pdm update np_workflows --no-self
+
+cmd /k
