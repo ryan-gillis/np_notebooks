d541e60aaed8c7b6aee80d396dbd2db85dece65b
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2023-10-04 15:34:27 -0700

    Speed-up update process [skip ci]

--- reset_update_launch.bat
+++ reset_update_launch.bat
@@ -25,8 +25,8 @@ git clean -f -d
 
 git pull origin main
 
-@REM python -m pip install -U np_workflows --extra-index-url https://pypi.org/simple --upgrade-strategy eager
-python -m pdm update np_workflows --no-self
+@REM python -m pip install -r requirements.txt
+python -m pdm sync
 
 start C:\JupyterLab\JupyterLab.exe
 
